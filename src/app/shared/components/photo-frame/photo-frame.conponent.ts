7e161198e3cae8de66c189a689469e18bf5b824f
ivanfsilva <ivanfs27@gmail.com>
2021-12-15 21:19:18 -0300

    Should create component test

--- src/app/shared/components/photo-frame/photo-frame.conponent.ts
+++ src/app/shared/components/photo-frame/photo-frame.conponent.ts
@@ -1,18 +1,35 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: 'photo-frame.component.html',
   styleUrls: ['photo-frame.component.scss']
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
   @Output() public liked: EventEmitter<void> = new EventEmitter();
   @Input() public description = '';
   @Input() public src = '';
   @Input() public likes = 0;
 
-  public like(): void {
-    this.liked.emit();
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
+  public ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe( debounceTime(500 ) )
+      .pipe( takeUntil( this.unsubscribe ) )
+      .subscribe( () => this.liked.emit() );
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  public like(): void {
+    this.debounceSubject.next();
+  }
 }
