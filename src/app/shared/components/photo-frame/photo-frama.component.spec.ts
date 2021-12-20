4a0608799da7c44aff6aa7f114a5069b5a1589c1
ivanfsilva <ivanfs27@gmail.com>
2021-12-20 08:59:37 -0300

    fakeAsync() and tick()

--- src/app/shared/components/photo-frame/photo-frama.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frama.component.spec.ts
@@ -1,6 +1,6 @@
-import {PhotoFrameComponent} from "./photo-frame.conponent";
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {PhotoFrameModule} from './photo-frame.module';
+import { PhotoFrameComponent } from './photo-frame.conponent';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { PhotoFrameModule } from './photo-frame.module';
 
 describe( PhotoFrameComponent.name, () => {
   let fixture: ComponentFixture<PhotoFrameComponent> = null;
@@ -18,4 +18,33 @@ describe( PhotoFrameComponent.name, () => {
   it(`Should create component`, () => {
     expect( component ).toBeTruthy();
   });
+
+  it(`#${ PhotoFrameComponent.prototype.like.name }
+    Should trigger ( @Output liked ) once when called
+    multiple times within debounce time`, fakeAsync( () => {
+
+    fixture.detectChanges();
+    let times = 0;
+
+    component.liked.subscribe( () => times++ );
+    component.like();
+    component.like();
+    tick(500);
+
+    expect( times ).toBe( 1 );
+  }));
+
+  it(`#${ PhotoFrameComponent.prototype.like.name }
+    Should trigger ( @Output liked ) two times when called
+    outside debounce time`, fakeAsync( () => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe( () => times++ );
+    tick(500);
+    component.like();
+    tick(500);
+    component.like();
+
+    expect( times ).toBe( 2 );
+  }));
 });
