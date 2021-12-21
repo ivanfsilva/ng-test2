3d7a66ab69cddc70aeb8b73b4071b7a9750c7e2e
ivanfsilva <ivanfs27@gmail.com>
2021-12-21 11:13:18 -0300

    appAction policy

--- src/app/shared/components/like-widget/like-widget.module.ts
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
+import { ActiondirectiveModule } from '../../directives/action/action.module';
 
 @NgModule({
   declarations: [LikeWidgetComponent],
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ActiondirectiveModule
   ],
   exports: [LikeWidgetComponent],
   providers: [UniqueIdService]
