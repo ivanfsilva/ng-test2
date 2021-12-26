ff3845e17ad433e5f05b059dc05c268fa85a05f8
ivanfsilva <ivanfs27@gmail.com>
2021-12-26 11:29:08 -0300

    refactoring the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
+
+const routes: Routes = [
+  { path: 'photos', component: PhotoListComponent },
+  { path: '**', redirectTo: 'photos' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
