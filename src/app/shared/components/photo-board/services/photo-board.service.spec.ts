b6a092802508084341a37b153fb92ee8727113ce
ivanfsilva <ivanfs27@gmail.com>
2021-12-26 15:04:04 -0300

    HttpClientTestingController

---
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PhotoBoardService } from './photo-board.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+describe(PhotoBoardService.name, () => {
+  let service: PhotoBoardService;
+  let httpController: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [PhotoBoardService],
+      imports: [HttpClientTestingModule]
+    }).compileComponents();
+
+    service = TestBed.inject(PhotoBoardService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+});
