6e06619364f96ac9ad808b7b7b076ed87b211595
ivanfsilva <ivanfs27@gmail.com>
2021-12-26 14:53:17 -0300

    HttpClientTestingModule

--- src/app/shared/components/photo-board/services/photo-board.service.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map, tap } from 'rxjs/operators';
 
 import { Photo } from '../interfaces/photo';
 
@@ -11,6 +11,13 @@ export class PhotoBoardService {
   constructor( private http: HttpClient ) {}
 
   public getPhotos(): Observable<Photo[]> {
-    return this.http.get<Photo[]>('http://localhost:3000/photos').pipe( delay(2000) );
+    return this.http.get<Photo[]>('http://localhost:3000/photos')
+      .pipe(map(photos => {
+        return photos.map(photo => {
+          return { ...photo, description: photo.description.toUpperCase() };
+        });
+      }))
+      .pipe(tap(photos => console.log(photos)))
+      .pipe( delay(2000));
   }
 }
